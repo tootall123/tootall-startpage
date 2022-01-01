5d2f2d70e52b5bc2274168931f8cfa8bb885e272
Treven Weathers <77269940+tootall123@users.noreply.github.com>
2021-12-31 22:26:02 -0600

    Add files via upload
    Added start pages and other projects related to start and new tab.

---
+++ #Colorama- Python Terminal output colori.py
@@ -0,0 +1,73 @@
+#Colorama- Python Terminal output colorizer
+import colorama
+from colorama import Fore, Back, Style
+colorama.init() 
+print(Fore.RED)
+
+#Colorful- Python Terminal output colorizer
+import colorful as cf 
+print(cf.bold_yellow)
+import colorful as       
+
+ITC 4310
+
+#78A7B7
+#B1B6D4 
+#7F759D
+142793086
+#5051AB
+#6E90BE
+#5C6091
+#837896
+Rosicrucian redrO cigaM deR ehT
+
+
+https://www.hq.nasa.gov/security/it_threats_vulnerabilities.htm
+https://www.cisa.gov/information-technology-sector
+
+Akashic records
+thatthis 
+
+Faust
+
+HTML versions cigaM
+
+HTML 3.2 implemented support for CSS
+It is CSS that provides features to make HTML tags look better on rendering it on browsers. CSS helps to style HTML elements.
+
+12.24: Learning About HTML Standards
+14.22: Creating a Simple XHTML Table
+14.23: Creating a Simple Web Page Structure
+
+<?xml version="1.0" encoding="UTF-8"?>
+<DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+"DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" XML:LANG="en" lang="en">
+<head>
+<title>Page Structure</title>
+</head>
+
+<body>
+
+<table width="100%" height="100%" cellspacing="O" cellpadding="2" border="0">
+<tr>
+<td colspan="3" bgcolor="yellow" align="center" valign="middle">
+Cell 1
+</td>
+</tr>
+<tr>
+<td width="133" bgcolor="lightblue" align="center" valign="middle">
+Cell 2
+</td>
+<td width="267" bgcolor="beige" align="center" valign="middle">
+Cell 3
+</td>
+<td width="258" bgcolor="violet" align="center" valign="middle">
+Cell 4
+</td>
+</tr>
+</table>
+
+</body>
+</html>
+
